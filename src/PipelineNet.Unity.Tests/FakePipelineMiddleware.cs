1ad52f839cd687e436660c651e7ce0ca8d894a5b
Shane Yu <shane.yu@iress.co.uk>
2018-07-11 16:32:54 +0100

    feat: added initial implementation

---
+++ src/PipelineNet.Unity.Tests/FakePipelineMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using PipelineNet.Middleware;
+
+namespace PipelineNet.Unity.Tests
+{
+    public class FakePipelineMiddleware : IMiddleware<FakeEntity>
+    {
+        private readonly IFakeService _fakeService;
+
+        public FakePipelineMiddleware(IFakeService fakeService)
+        {
+            _fakeService = fakeService;
+        }
+
+        /// <inheritdoc />
+        public void Run(FakeEntity entity, Action<FakeEntity> next)
+        {
+            _fakeService.DoubleNumber(entity);
+            next?.Invoke(entity);
+        }
+    }
+}
---
+++ src/PipelineNet.Unity/UnityMiddlewareResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using PipelineNet.MiddlewareResolver;
+
+using Unity;
+
+namespace PipelineNet.Unity
+{
+    public class UnityMiddlewareResolver : IMiddlewareResolver
+    {
+        private readonly IUnityContainer _unityContainer;
+
+        public UnityMiddlewareResolver(IUnityContainer unityContainer)
+        {
+            _unityContainer = unityContainer;
+        }
+
+        /// <inheritdoc />
+        public object Resolve(Type type)
+        {
+            return _unityContainer.Resolve(type);
+        }
+    }
+}
